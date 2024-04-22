b81a0d3fe087f402a3b718429eca2d8c0c44484c
Kat Tsysarenko <61129855+katerynatsy@users.noreply.github.com>
2024-04-22 12:26:44 +1000

    Initial Commit

---
+++ InboundCalls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InboundCalls
+//
+//  Created by Kat on 22/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InboundCalls/InboundCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InboundCallsApp.swift
+//  InboundCalls
+//
+//  Created by Kat on 22/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct InboundCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
